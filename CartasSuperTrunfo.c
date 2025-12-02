efe0995b5c17558c525681e1f08b89ba2f645f35
RenatoRanieri <ranieriguerreiro@gmail.com>
2025-12-02 16:20:59 +0000

    criando jogo de cartas

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -2,9 +2,33 @@
 
 // Desafio Super Trunfo - Países
 int main(){
+    // Carta 1
+    char estado1;
+    char codigo1[4];
+    char nomeCidade1[50];
+    int populacao1;
+    float area1;
+    float pib1;
+    int pontosTuristicos1;
+
+    // Carta 2
+    char estado2;
+    char codigo2[4];
+    char nomeCidade2[50];
+    int populacao2;
+    float area2;
+    float pib2;
+    int pontosTuristicos2;
+
     printf("#####################################\n");
     printf("# Bem-vindo ao Super Trunfo-Países! #\n");
     printf("#####################################\n");
+
+    // Entrada dos dados da Carta 1
+    printf("Digite o Estado: ");
+    scanf(" %c", &estado1);
+
+
     
     return 0;
 } 
